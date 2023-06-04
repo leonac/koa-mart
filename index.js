e0dba8019c1052b561a760c23fb7c963cea203b6
leonac <xiang516@gmail.com>
2023-06-04 15:26:27 +0800

    add api topics

--- index.js
+++ index.js
@@ -50,6 +50,69 @@ router.get("/api/wx_openid", async (ctx) => {
   }
 });
 
+
+
+// 获取计数
+router.get("/api/topics", async (ctx) => {
+  const result = await Counter.count();
+
+  ctx.body = {
+    "msg": "请求成功",
+    "data":
+    {
+        "totalPage": 1,
+        "posts":
+        [
+            {
+                "postId": 1,
+                "title": "中古赶海市集",
+                "time": "周一",
+                "address": "大悦城F1",
+                "description": "复古/创意"
+            },
+            {
+                "postId": 2,
+                "time": "周六和周日",
+                "title": "凤凰艺市",
+                "address": "西湖艺创小镇",
+                "description": "创意/手工"
+            },
+            {
+                "postId": 3,
+                "time": "周三",
+                "title": "2023大屋顶 怂人胆X东海溜儿节",
+                "address": "良渚文化艺术中心",
+                "description": "语言/话剧"
+            },
+            {
+                "postId": 4,
+                "time": "周三至周五",
+                "title": "大可露营夏日市集",
+                "address": "西湖区灵龙路586号田野和季风",
+                "description": "露营/户外"
+            },
+            {
+                "postId": 5,
+                "time": "周一至周日",
+                "title": "青山集",
+                "address": "青山村 访客中心",
+                "description": "手工/创意/户外/舞台"
+            }
+        ],
+        "currentPage": 1
+    },
+    "code": 0
+  };
+});
+
+
+
+
+
+
+
+
+
 const app = new Koa();
 app
   .use(logger())
